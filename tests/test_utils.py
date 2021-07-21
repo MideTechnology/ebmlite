39877810d7e15a784c3b19e767ce8f0df2004519
Becker Awqatty <bawqatty@mide.com>
2021-07-21 15:06:21 -0400

    fixed tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,9 +8,9 @@ SCHEMA_PATH = os.path.join(".", "ebmlite", "schemata", "matroska.xml")
 
 @pytest.mark.script_launch_mode('subprocess')
 def test_ebml2xml(script_runner):
-    path_base = os.path.join(".", "tests", "video-1{ext}")
-    path_in = path_base.format(ext=".mkv")
-    path_out = path_base.format(ext=".mkv.xml")
+    path_base = os.path.join(".", "tests", "video-4{ext}")
+    path_in = path_base.format(ext=".ebml")
+    path_out = path_base.format(ext=".ebml.xml")
     path_expt = path_base.format(ext=".xml")
 
     result = script_runner.run(
@@ -18,6 +18,7 @@ def test_ebml2xml(script_runner):
         path_in,
         SCHEMA_PATH,
         "--output=" + path_out,
+        "--clobber",
     )
     assert result.success
 
@@ -34,16 +35,17 @@ def test_ebml2xml(script_runner):
 
 @pytest.mark.script_launch_mode('subprocess')
 def test_xml2ebml(script_runner):
-    path_base = os.path.join(".", "tests", "video-1{ext}")
+    path_base = os.path.join(".", "tests", "video-4{ext}")
     path_in = path_base.format(ext=".xml")
     path_out = path_base.format(ext=".xml.ebml")
-    path_expt = path_base.format(ext=".mkv")
+    path_expt = path_base.format(ext=".ebml")
 
     result = script_runner.run(
         "python", "-m", "ebmlite.util", "xml2ebml",
         path_in,
         SCHEMA_PATH,
         "--output=" + path_out,
+        "--clobber",
     )
     assert result.success
 
