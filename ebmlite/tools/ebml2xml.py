71e2aec4903d7fb281366e41b18c08c8a1ab699f
David Stokes <dstokes@mide.com>
2022-01-11 11:32:39 -0500

    Undid tool changes (not meant for committing

--- ebmlite/tools/ebml2xml.py
+++ ebmlite/tools/ebml2xml.py
@@ -36,20 +36,8 @@ def main():
         action="store_true",
         help="Generate XML with maximum description, including offset, size, type, and id info",
     )
-    argparser.add_argument(
-        '-l', '--list', action="store_true",
-        help="Display a list of EBML schemata found in SCHEMA_PATH",
-    )
-    argparser.add_argument(
-        '-L', '--list_relative', action="store_true",
-        help="Display a list of EBML schemata found in SCHEMA_PATH, using package-relative filenames",
-    )
 
     args = argparser.parse_args()
-
-    if args.list:
-        ebmlite.util.printSchemata(args.output, absolute=not args.list_relative)
-        exit(0)
 
     with utils.load_files(args, binary_output=args.single) as (schema, out):
         doc = schema.load(args.input, headers=True)
