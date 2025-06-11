9df81dae00e8b846401b999551bed90654cec859
stokesMIDE <dstokes@mide.com>
2025-06-11 18:21:59 -0400

    Added basic Sphinx docs (some from ancient branch, needs updating)

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ebmlite'
+copyright = '2025, David R. Stokes'
+author = 'David R. Stokes'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,34 @@
+.. ebmlite documentation master file, created by
+   sphinx-quickstart on Wed Jul  8 15:45:54 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ebmlite's documentation!
+===================================
+
+EBMLite: A lightweight EBML parsing library. It is designed to crawl through EBML
+files quickly and efficiently, and that’s about it.
+
+.. .. toctree::
+..    :maxdepth: 2
+..    :caption: Readme:
+
+..    readme
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Modules:
+
+   ebmlite/core
+   ebmlite/decoding
+   ebmlite/encoding
+   ebmlite/util
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
